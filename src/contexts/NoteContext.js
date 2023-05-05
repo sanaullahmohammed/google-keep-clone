c74a0c3d4e280697cc074229ceef8b1bf76d2961
sanaullahmohammed <sanaullah.mohammad100@gmail.com>
2023-05-05 11:49:32 +0530

    add localStorage function

--- src/contexts/NoteContext.js
+++ src/contexts/NoteContext.js
@@ -1,10 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { v4 as uuid } from "uuid";
 
 const NotesContext = React.createContext();
 
 const NotesContextProvider = ({ children }) => {
   const [notes, setNotes] = useState([]);
+
+  //apply the save and get functions using useEffect
+  //get the saved notes and add them to the array
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("Notes"));
+    if (data) {
+      setNotes(data);
+    }
+  }, []);
+
+  //saving data to local storage
+  useEffect(() => {
+    localStorage.setItem("Notes", JSON.stringify(notes));
+  }, [notes]);
 
   // addNote function
   const addNote = ({ title, content }) => {
